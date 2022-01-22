77b8a2916819e205250c53b93dd8cd898ac2c567
githubityu <longyujun0624@live.cn>
2022-01-22 17:26:12 +0800

    first

--- lib/local/constants.dart
+++ lib/local/constants.dart
@@ -18,6 +18,7 @@ class Constants {
   static const String accessToken = 'accessToken';
   static const String refreshToken = 'refreshToken';
 
+
   static const String theme = 'AppTheme';
   static const String locale = 'locale';
   static const String appTheme = 'appTheme';
